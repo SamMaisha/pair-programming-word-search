e317361db0d10875e39a2c4f0b5dc296c46eb7d7
Sam Maisha <sam.maisha99@gmail.com>
2022-12-16 08:37:06 +0000

    implemented wordsearch fuction

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// function to get vertical array elements from an array of arrays
+const transpose = function(matrix) {
+    let arrayOfArrays = [];
+    let i = 0;
 
-module.exports = wordSearch
+    for (let rowIndex = 0; rowIndex < matrix[0].length; rowIndex++) {
+        i = rowIndex;
+        const array = [];
+        arrayOfArrays.push(array);
+        for (let row = 0; row < matrix.length; row++) {
+            array.push(matrix[row][i]);
+        }
+    }
+    return arrayOfArrays;
+};
+
+const wordSearch = (letters, word) => {
+    // looking for words horizontally on letters array
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    // looking for words vertically on the letters array
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+module.exports = wordSearch;
